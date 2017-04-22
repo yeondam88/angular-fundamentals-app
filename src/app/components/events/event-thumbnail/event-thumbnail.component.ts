7a8ee3e77ee5dc5f457fc0bdc472399e7eef8aeb
Yeondam Park <giyen9088@gmail.com>
2017-04-21 22:11:46 -0700

    Add data and style to component

---
+++ src/app/components/events/event-thumbnail/event-thumbnail.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-event-thumbnail',
+  templateUrl: './event-thumbnail.component.html',
+  styleUrls: ['./event-thumbnail.component.css']
+})
+export class EventThumbnailComponent implements OnInit {
+
+  @Input() event: any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
